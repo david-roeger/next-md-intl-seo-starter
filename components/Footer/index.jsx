2d039a9fd131d31354a4c24d18fb4311722da87c
David Röger <david@codeatelier.com>
2022-03-22 23:01:51 +0100

    Add json translation support

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Link as PrimerLink } from "@primer/react";
 import { useRouter } from "next/router";
 import { LOCALES, SITE_URL } from "../../lib/constants";
+import { t } from "../../lib/translation";
 
 const Footer = ({}) => {
   const router = useRouter();
@@ -10,9 +11,7 @@ const Footer = ({}) => {
     <footer>
       © 2022 Thomas Hartwig{" "}
       <Link href={`${SITE_URL}/imprint`} locale={router.locale} passHref>
-        <PrimerLink>
-          {router.locale === LOCALES.DE ? "Impressum" : "imprint"}
-        </PrimerLink>
+        <PrimerLink>{t("TRANSLATION_IMPRINT", router.locale)}</PrimerLink>
       </Link>
     </footer>
   );
