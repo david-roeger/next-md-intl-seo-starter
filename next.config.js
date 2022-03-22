b3a8122e9846c8fa06e421e628c322209d92893c
David Röger <david@codeatelier.com>
2022-03-22 22:27:47 +0100

    Add md loader

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ module.exports = {
     locales: ["en", "de"],
     defaultLocale: "en",
   },
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
 };
